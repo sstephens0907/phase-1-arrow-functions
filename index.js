333db9abf09d7a86d6c0d76a4ee35a57a63f5e94
Sarah Stephens <sarahstephens84@gmail.com>
2022-09-25 13:04:04 -0400

    commit tree d43b7a835c343e3fa9afc98c36f4af464045ddb8

---
+++ index.js
@@ -0,0 +1,10 @@
+const divide = function(parameter1, parameter2){
+    return parameter1/parameter2;
+}
+divide(2000/100)
+
+const square = (param1) => param1 * param1;
+square(2)
+
+const add = (num1, num2) => num1 + num2;
+add(3,4)
